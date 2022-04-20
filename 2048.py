38aca371dd9bae2f6065522e05c019ff49cbac06
Archer Wayne <archerwayne1216@gmail.com>
2022-04-20 22:25:01 +0800

    update

--- 2048.py
+++ 2048.py
@@ -2,47 +2,75 @@ import pygame
 import sys
 import numpy as np
 
+from debug import debug
+
 screen_size = (320, 370)
 block_size = (60, 60)
-block_location = []
-array2048 = np.array([[0, 0, 0, 0],
+# font = pygame.font.Font('Bold', 50)
+block_locations = [(10, 60), (90, 60), (170, 60), (250, 60), \
+                  (10, 140), (90, 140), (170, 140), (250, 140), \
+                  (10, 220), (90, 220), (170, 220), (250, 220), \
+                  (10, 300), (90, 300), (170, 300), (250, 300)]
+# [1 2 3 4]
+# [5 6 7 8]
+# [9 10 11 12]
+# [13 14 15 16]
+# 访问列表中的值: 用方括号
+
+array2048_2 = np.array([[0, 0, 0, 0],
                       [0, 0, 0, 0],
                       [0, 0, 0, 0],
                       [0, 0, 0, 0],
                       ])
 
+array2048_1 = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 
-# class Block(pygame.sprite.Sprite):
-#     def __init__(self, groups, number, location):
-#         super(Block, self).__init__(groups)
-#         self.number = number
-#         self.location = location
-#         self.image = pygame.Surface(block_size)
-#         self.image.fill('#abcdef')
-#         self.rect = self.image.get_rect(topleft=self.location)
-#
-#
-#     def update(self):
-#         pass
+colors = {0: '#dadbd9', 2: '#ced9bd', 4: '#bbd989', 8: '#aad959', 16: '#78ab20', 32: '#619605', 64: '#cf765b',
+          128: '#cc5531', 256: '#cf451b', 512: '#c2360c', 1024: '#8f2100', 2048: '#fce805', \
+          4096: '#ad9f02', 8192: '#827700', 16384: '#2b4bff', 32768: '#7033ff', 65536: '#cc0cf2', 131072: '#000000'}
+
+
+# 访问字典里的值: 把相应的键放入到方括号中
+
+class Block(pygame.sprite.Sprite):
+    def __init__(self, groups, sequence):
+        super(Block, self).__init__(groups)
+        self.sequence = sequence
+        self.number = 0
+
+    def update(self, array, colors, block_locations):
+        self.number = array[self.sequence]
+        self.image = pygame.Surface(block_size)
+        self.image.fill(colors[self.number])
+        # self.image.fill('#654321')
+        self.rect = self.image.get_rect(topleft=block_locations[self.sequence])
+
 
 
 # group setup
-# block_sprites = pygame.sprite.Group()
-# block = Block(block_sprites, 2048, (33, 33))
+block_sprites = pygame.sprite.Group()
+block_list = []
+for i in range(16):
+    block_list.append(Block(block_sprites, i))
+
 
 pygame.init()
 screen = pygame.display.set_mode(screen_size)
 
-def number_move(): # 要改成press down
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w] or keys [pygame.K_UP]: # up
-        print('up')
-    if keys[pygame.K_s] or keys [pygame.K_DOWN]: # down
-        pass
-    if keys[pygame.K_a] or keys [pygame.K_LEFT]: # left
-        pass
-    if keys[pygame.K_d] or keys [pygame.K_RIGHT]: # down
-        pass
+
+# get_pressed 和 event检测是实现两种不同功能的方式, pygame.KEYDOWN是检测按下事件, 而pygame.key.get_pressed是检测按键长按的
+def block_move(array):
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN and (event.key == pygame.K_w or event.key == pygame.K_UP):
+            pass
+        if event.type == pygame.KEYDOWN and (event.key == pygame.K_s or event.key == pygame.K_DOWN):
+            pass
+        if event.type == pygame.KEYDOWN and (event.key == pygame.K_a or event.key == pygame.K_LEFT):
+            pass
+        if event.type == pygame.KEYDOWN and (event.key == pygame.K_d or event.key == pygame.K_RIGHT):
+            pass
+
+
 
 def main():
     while True:
@@ -52,11 +80,12 @@ def main():
                 sys.exit()
 
         screen.fill('#123456')
-        # block_sprites.update()
-        # block_sprites.draw(screen)
-        number_move()
+        block_sprites.update(array2048_1, colors, block_locations)
+        block_sprites.draw(screen)
+        # debug('i', 10, 10)
 
-        pygame.display.update()
+        pygame.display.update() # 这一句话必须放在最后面
+
 
 
 if __name__ == "__main__":
