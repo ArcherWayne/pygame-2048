8c4b2ff072a24754b50c7d08e3f8ec1b143f455d
Archer Wayne <archerwayne1216@gmail.com>
2022-06-01 21:53:05 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,21 +1,5 @@
-def append_list():
-    list.append('小毛')
+list = [0, 1, 2, 3, 4, 5]
+print(len(list))
 
-def assign_list():
-    list = ['嘿几把小毛']
-
-def assign_list_returned(list):
-    list = ["小毛嘿几把"]
-    return list
-
-list = ['嘿', '几', '吧']
-print(list)
-
-append_list()
-print(list)
-
-assign_list()
-print(list)
-
-list2 = assign_list_returned(list)
-print(list2)
+for x in range(len(list)):
+    print(list[x])
